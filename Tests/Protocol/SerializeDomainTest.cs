5ee65b277a6947bb4e7606d1c399e4b51a0170c5
Mirza Kapetanovic <mirza.kapetanovic@gmail.com>
2014-09-14 21:56:16 +0200

    Added domain tests

---
+++ Tests/Protocol/SerializeDomainTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using DNS.Protocol;
+
+namespace DNS.Tests.Protocol {
+    [TestFixture]
+    public class SerializeDomainTest {
+        private static string[] GetArray(params string[] strings) {
+            return strings;
+        }
+
+        [Test]
+        public void EmptyDomain() {
+            Domain domain = new Domain(GetArray());
+            byte[] content = Helper.ReadFixture("Domain", "empty-label");
+
+            CollectionAssert.AreEqual(content, domain.ToArray());
+        }
+
+        [Test]
+        public void DomainWithSingleLabel() {
+            Domain domain = new Domain(GetArray("www"));
+            byte[] content = Helper.ReadFixture("Domain", "www-label");
+
+            CollectionAssert.AreEqual(content, domain.ToArray());
+        }
+
+        [Test]
+        public void DomainWithMultipleLabels() {
+            Domain domain = new Domain(GetArray("www", "google", "com"));
+            byte[] content = Helper.ReadFixture("Domain", "www.google.com-label");
+
+            CollectionAssert.AreEqual(content, domain.ToArray());
+        }
+    }
+}
