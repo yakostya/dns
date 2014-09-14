41901215851c0585f4eee6ffe0ffed02001878b4
Mirza Kapetanovic <mirza.kapetanovic@gmail.com>
2014-09-14 13:35:31 +0200

    Initial unit tests using NUnit. Header parsing and serialization tests.

---
+++ Tests/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace DNS.Tests {
+    public static class Helper {
+        private static readonly string BASE_DIRECTORY = 
+            Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.FullName;
+
+        public static byte[] ReadFixture(params string[] paths) {
+            string path = Path.Combine(paths);
+            path = Path.Combine(BASE_DIRECTORY, "Fixtures", path);
+
+            return File.ReadAllBytes(path);
+        }
+    }
+}
